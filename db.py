b212945f8a1d784664cf634d42372e87b1cd9478
yaroslavver <gamesd1358@gmail.com>
2022-04-20 21:47:05 +0300

    test4

--- db.py
+++ db.py
@@ -1,3 +1,18 @@
-def ok():
-    return True
-print("wow")
+filer = open('baza.txt', 'r')
+filew = open('baza.txt', 'w')
+class b():
+    global file
+    def add(h1, h2):
+        x = str(h1)+"#"+str(h2)+"\n"
+        filew.write(x)
+        filew.close()
+        return
+    def read(h1):
+        x = (filer.read()).split('\n')
+        info = ''
+        for i in range (len(x)):
+            if str(h1) in x[i]:
+                info += str(x[i])
+        return info
+b.add('pc', 'ui')
+print(b.read('pc'))
